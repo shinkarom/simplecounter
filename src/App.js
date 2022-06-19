dd9045491f3bbbf2971724a95eefcb3f8dd3d79b
shinkarom <shinkarom@gmail.com>
2022-06-19 20:53:24 +0300

    Implement counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
-//import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { styled } from '@stitches/react'
 
 const App = () => {
 	
+	const [counter, setCounter] = React.useState(0);
+	
+	const increment = function(){setCounter(counter+1)}
+	const decrement = function(){setCounter(counter-1)}
+	const reset = function(){setCounter(0)}
 	
   return (
-		<>
-      <h3>React App</h3>
-		</>
+		<GlobalDiv>
+      <h3>Simple Counter</h3>
+	  <CounterShower number={counter} />
+	  <Button onClick={decrement}>Decrement</Button>
+	  <Button onClick={reset}>Reset</Button>
+	  <Button onClick={increment}>Increment</Button>
+		</GlobalDiv>
   );
 };
+
+const GlobalDiv = styled("div", {
+		'margin-left': 'auto',
+		'margin-right': 'auto',
+		'width': '50%'
+	});
+
+function CounterShower(props){
+	
+	const MySpan = styled("span", {
+		'font-size': '30px',
+		});
+	
+	return (
+	<div>
+		<MySpan>{props.number}</MySpan>
+	</div> )
+	
+}
 
 export default App;
